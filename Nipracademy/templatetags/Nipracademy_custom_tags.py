5603697e94b250afec05f8500b8b18ecaf216607
vivek kumar <104557123+vivek85410@users.noreply.github.com>
2024-08-09 00:25:11 +0530

    Add files via upload

---
+++ Nipracademy/templatetags/Nipracademy_custom_tags.py
@@ -0,0 +1,33 @@
+from django import template
+import math
+
+from Nipracademy.models import Course ,UserCourse
+register = template.Library()
+
+# 100 -> 10% --> mrp  - ( mrp * discount * 0.01 ) = selprice
+@register.simple_tag
+def cal_sellprice(price , discount):
+    if discount is None or discount is 0:
+        return price
+    sellprice = price
+    sellprice = price - ( price * discount * 0.01 )
+    return math.floor(sellprice)
+
+
+@register.filter
+def rupee(price):
+    return f'₹{price}'
+
+@register.simple_tag
+def is_enrolled(request , course):
+   
+    user = None
+    if not request.user.is_authenticated:
+        return False
+        # if you are enrooled in this course you can watch every video
+    user = request.user
+    try:
+        user_course = UserCourse.objects.get(user = user  , course = course)
+        return True
+    except:
+        return False
